9850f5f86215fd2883c5d2c624c2dcb4731d6ad1
MSzweda <szweda.malgorzata+github@gmail.com>
2019-08-20 20:09:31 +0200

    switch to submodule and add some empty templates

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # face-rec
+
+# Setup
+
+    git submodule init
+    git submodule update
+
+    yarn install
+    yarn start
+
+## Libs
+
+https://github.com/justadudewhohacks/face-api.js
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const reload = require('reload');
+const path = require('path');
+const watch = require('watch');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const viewsDir = path.join(__dirname, 'views');
+const faceapiExamples = path.join(__dirname, 'face-api.js/examples/examples-browser/');
+app.use(express.static(viewsDir));
+app.use(express.static(path.join(faceapiExamples, './public')));
+app.use(express.static(path.join(faceapiExamples, '../images')));
+app.use(express.static(path.join(faceapiExamples, '../media')));
+app.use(express.static(path.join(faceapiExamples, '../../weights')));
+app.use(express.static(path.join(faceapiExamples, '../../dist')));
+
+app.get('/', (req, res) => res.sendFile(path.join(viewsDir, 'main.html')));
+
+const server = http.createServer(app);
+reload(app).then(function (reloadReturned) {
+  watch.watchTree(viewsDir, function (f, curr, prev) {
+    // reloadReturned.reload();
+  });
+
+  server.listen(app.get('port'), function () {
+    console.log(`Web server listening on port ${app.get('port')}`)
+  })
+}).catch(function (err) {
+  console.error('Reload could not start, could not start server app', err)
+});
+
